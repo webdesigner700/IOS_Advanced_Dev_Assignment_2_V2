e3ce9981ccefbccd79dd8af1f8520fa8a2f9bcd8
Vinay B <vinay.bayyapunedi@gmail.com>
2023-09-07 17:31:13 +1000

    Initial Commit

---
+++ IOS_Advanced_Assignment_2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IOS_Advanced_Assignment_2
+//
+//  Created by vinay bayyapunedi on 07/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS_Advanced_Assignment_2/IOS_Advanced_Assignment_2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Advanced_Assignment_2App.swift
+//  IOS_Advanced_Assignment_2
+//
+//  Created by vinay bayyapunedi on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Advanced_Assignment_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
