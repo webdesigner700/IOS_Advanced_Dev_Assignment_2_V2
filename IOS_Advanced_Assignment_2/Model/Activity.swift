7c293c72798eb7c6c11b4f0ebddfadb4f3976b30
Vinay B <vinay.bayyapunedi@gmail.com>
2023-09-08 11:25:46 +1000

    Created a structure that represents an Activity and created the load function to load in data from a JSON file into an Array. Added a JSON file that has information about San Francisco Activities.

--- IOS_Advanced_Assignment_2/Model/Activity.swift
+++ IOS_Advanced_Assignment_2/Model/Activity.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Activity: Hashable, Codable, Identifiable {
+    
+    var id: Int
+    var name: String
+    var city: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    var activityCoordinate: CLLocationCoordinate2D {
+            CLLocationCoordinate2D(
+                latitude: coordinates.latitude,
+                longitude: coordinates.longitude
+            )
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
