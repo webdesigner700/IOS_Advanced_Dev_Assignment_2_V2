18d0f28c3b825d3bcf99905b3aca446ad1867546
Vinay B <vinay.bayyapunedi@gmail.com>
2023-09-17 10:43:45 +1000

    Created comments for Activity and Accommodation Model files

--- IOS_Advanced_Assignment_2/Model/Accomodation.swift
+++ IOS_Advanced_Assignment_2/Model/Accomodation.swift
@@ -9,21 +9,31 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
+// A struct named "Accomodation" that conforms to the Hashable, Codable and Identifiable Swift protocols is created.
+
 struct Accomodation: Hashable, Codable, Identifiable {
     
-    var id: Int
-    var name: String
-    var district: String
-    var city: String
-    var description: String
+    // The purpose of this Accomodation struct is to create a model that can hold data about a specific accomodation once data from the "AccomodationData" JSON file is loaded into an array of the type Accomodation
     
-    private var imageName: String
+    var id: Int // A unique identifier for each Accomodation
+    var name: String // The name of the accomodation
+    var district: String // The city ewhere the accomodation is located
+    var city: String // The state where the accomodation is located
+    var description: String // A description about the accomodation
+    
+    private var imageName: String // The name of the image associated wqith the accomodation
+    
+    // Computed property to create an "Image" View from the "imageName" property
     
     var image: Image {
         Image(imageName)
     }
     
+    // A variable of the type "Coordinates" to hold the latitude and longitude coordinates of the accomodation
+    
     private var coordinates: Coordinates
+    
+    // Computed property to convert 'coordinates" into a variable "accomodationCoordinate" of the type "CLLocationCoordinate2D"
     
     var accomodationCoordinate: CLLocationCoordinate2D {
             CLLocationCoordinate2D(
@@ -32,10 +42,12 @@ struct Accomodation: Hashable, Codable, Identifiable {
             )
     }
     
+    // A nested struct called "Coordinates" is created that represents the latitude and longitude of the accomodation. The variable "coordinates" on line 32 conforms to this structure
+    
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
     }
     
-    var inItinerary: Bool
+    var inItinerary: Bool // A Boolean indicating whether the accomodation is added to the Itinerary or not.
 }
--- IOS_Advanced_Assignment_2/Model/Activity.swift
+++ IOS_Advanced_Assignment_2/Model/Activity.swift
@@ -9,21 +9,31 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
+// A struct named "Activity" that conforms to the Hashable, Codable and Identifiable Swift protocols is created.
+
 struct Activity: Hashable, Codable, Identifiable {
     
-    var id: Int
-    var name: String
-    var city: String
-    var state: String
-    var description: String
+    // The purpose of this Activity struct is to create a model that can hold data about a specific activity once data from the "ActivityData" JSON file is loaded into an array of the type Activity
     
-    private var imageName: String
+    var id: Int // A unique identifier for each activity
+    var name: String // The name of the activity
+    var city: String // The city where the activity is located
+    var state: String // The state where the activity is located
+    var description: String // A description about the activity
+    
+    private var imageName: String // The name of the image associated with the activity
+        
+    // Computed property to create an "Image" View from the "imageName" property
     
     var image: Image {
         Image(imageName)
     }
     
+    // A variable of the type "Coordinates" to hold the latitude and longitude coordinates of the activity
+    
     private var coordinates: Coordinates
+    
+    // Computed property to convert 'coordinates" into a variable "activityCoordinate" of the type "CLLocationCoordinate2D"
     
     var activityCoordinate: CLLocationCoordinate2D {
             CLLocationCoordinate2D(
@@ -32,10 +42,12 @@ struct Activity: Hashable, Codable, Identifiable {
             )
     }
     
+    // A nested struct called "Coordinates" is created that represents the latitude and longitude of the activity. The variable "coordinates" on line 32 conforms to this structure
+    
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
     }
     
-    var inItinerary: Bool
+    var inItinerary: Bool // A Boolean indicating whether the activity is added to the Itinerary or not.
 }
